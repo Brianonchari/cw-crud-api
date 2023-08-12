a8a26c8325e54a8a40c99c0b046dafe70ffcb66b
Brianonchari <oncharibrian@gmail.com>
2023-08-12 12:47:26 +0300

    integrate openai

---
+++ Controllers/GPTController.cs
@@ -0,0 +1,30 @@
+﻿using cw.applicationservice.addproduct;
+using cw.applicationservice.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw.Controllers
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public class GPTController : ControllerBase
+    {
+        private readonly IADProductService _productService;
+        public GPTController(IADProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AdProductResponseModel>> generateContent(CustomerRequestModel customerRequest)
+        {
+            try
+            {
+                var response = await _productService.GenerateAdContent(customerRequest);
+                return response;
+            }catch(System.Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using cw.applicationservice.addproduct;
+using cw.applicationservice.interfaces;
 using cw.Helpers;
+using cw.infrastructure.network;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IADProductService, ADProductService>();
+builder.Services.AddScoped<IBotAPIService, BotApiService>();
 string? connectionString = builder.Configuration.GetConnectionString("DBConn");
 builder.Services.AddDbContext<CWDbContext>(options =>{
     options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
